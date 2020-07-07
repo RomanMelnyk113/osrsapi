a9395dd0a3a4732d1242be492e0b80f5979b29a3
Roman Melnyk <roman.melnyk20071992@gmail.com>
2020-07-07 11:26:09 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3:7',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords='osrs grand_exchange osrsapi python',
