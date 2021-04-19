59dfe589f8e9933de84f170ffbf0734a1b7d47ae
Roman Melnyk <roman.melnyk20071992@gmail.com>
2021-04-19 23:09:42 +0300

    add debug logging for grandexchange

--- osrsapi/grandexchange.py
+++ osrsapi/grandexchange.py
@@ -38,6 +38,7 @@ class GrandExchange:
             raise GameItemNotFound(f"Unable to find item from {db_name} with id {id}.")
 
         try:
+            logger.info("Item response", response.text)
             json_data = response.json()
             item = json_data.get("item")
         except JSONDecodeError as e:
@@ -49,6 +50,7 @@ class GrandExchange:
             raise GameItemParseError(msg)
 
         try:
+            logger.info("Item response_graph", response_graph.text)
             graph_json_data = response_graph.json()
             daily_data = graph_json_data.get("daily")
         except JSONDecodeError as e:
